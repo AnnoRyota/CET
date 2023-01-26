6bed61cfda8da1ea771da3eba621f01d975de238
Anno Ryota <anno1216.rad@gmail.com>
2023-01-26 17:13:42 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundSize: {
+        'full': '150%',
+      }
+    },
+  },
+  plugins: [],
+}
